92218e0c88df0a7d319055a78d5a568eb590100f
LarryWatterson <49096147+LarryWatterson@users.noreply.github.com>
2022-11-26 23:22:01 +0300

    Update lwt_bossevent.cpp

--- lwt_bossevent.cpp
+++ lwt_bossevent.cpp
@@ -3,22 +3,22 @@
 #ifdef ENABLE_BOSS_EVENT
 #include "lwt_bossevent.h"
 #include "char_manager.h"
-// #define LOG_TUT
+// #define LOGS
 
-void CAutoBossEvent::EkleBunu(LPCHARACTER ch)
+void CAutoBossEvent::AddPlayer(LPCHARACTER ch)
 {
 	vec_player.emplace_back(ch);
-#ifdef LOG_TUT
-	sys_err("Boss Event giren oyuncu : '%s' vec boyut : %d", ch->GetName(), vec_player.size());
+#ifdef LOGS
+	sys_err("Boss Event player : '%s' vec size : %d", ch->GetName(), vec_player.size());
 #endif
 }
 
-void CAutoBossEvent::BossSpawnla()
+void CAutoBossEvent::SpawnBoss()
 {
 	for(const auto& it : vec_player)
 	{
 		CHARACTER_MANAGER::instance().SpawnMob(BOSS_EVENT_VNUM, BOSS_EVENT_MP, it->GetX(), it->GetY(), 0, false, 360);
 	}
-	vec_player.clear(); //bb
+	vec_player.clear();
 }
 #endif
